e53ff0964f4050127fcf52b1e9c8490d81872080
Midjan <midjankacirani@gmail.com>
2022-07-23 13:38:15 +0200

    commit

--- src/main/java/com/example/final_project/FinalProjectApplication.java
+++ src/main/java/com/example/final_project/FinalProjectApplication.java
@@ -3,6 +3,12 @@ package com.example.final_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring application as the final project of LHIND intership
+ * @author Midjan Kaçirani
+ * @since JUL-2022
+ * @version 1
+ */
 @SpringBootApplication
 public class FinalProjectApplication {
 
