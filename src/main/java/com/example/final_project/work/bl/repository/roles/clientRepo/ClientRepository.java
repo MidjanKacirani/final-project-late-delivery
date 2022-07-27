db8f5aa6f0c99761519ab6d54b182e28c78913ba
Midjan <midjankacirani@gmail.com>
2022-07-27 16:22:34 +0200

    commit

--- src/main/java/com/example/final_project/work/bl/repository/roles/clientRepo/ClientRepository.java
+++ src/main/java/com/example/final_project/work/bl/repository/roles/clientRepo/ClientRepository.java
@@ -1,9 +1,9 @@
 package com.example.final_project.work.bl.repository.roles.clientRepo;
 
-import com.example.final_project.work.bl.model.entity.user.User;
+import com.example.final_project.work.bl.model.entity.restaurant.RestaurantMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<User, Long> {
+public interface ClientRepository extends JpaRepository<RestaurantMenu, Long> {
 }
--- src/main/java/com/example/final_project/work/bl/services/ClientService.java
+++ src/main/java/com/example/final_project/work/bl/services/ClientService.java
@@ -1,4 +1,21 @@
 package com.example.final_project.work.bl.services;
 
+import com.example.final_project.work.bl.model.entity.order.OrderDetails;
+import com.example.final_project.work.bl.model.entity.restaurant.Restaurant;
+import com.example.final_project.work.bl.model.entity.restaurant.RestaurantMenu;
+import com.example.final_project.work.bl.model.enums.STATUS;
+
+import java.util.List;
+
 public interface ClientService {
+
+    List<RestaurantMenu> retrieveAllActiveMenu();
+
+    Boolean order(RestaurantMenu restaurantMenu);
+
+    STATUS checkOrderStatus(OrderDetails id);
+
+    List<OrderDetails> seeAllMyOrders();
+
+
 }
